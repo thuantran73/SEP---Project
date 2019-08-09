bbd6a022fdc872cadb0880bf6c7efdf23e9a60f5
thuantran73 <larten1234@gmail.com>
2019-08-09 10:51:35 +0700

    QR code

--- app/src/main/java/com/larten/sep_project/MainActivity.java
+++ app/src/main/java/com/larten/sep_project/MainActivity.java
@@ -2,12 +2,72 @@ package com.larten.sep_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnClick;
+    TextView txtName , txtDiachi;
+    ImageView imgHinh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClick = (Button)findViewById(R.id.btnClick);
+        txtName = (TextView)findViewById(R.id.txtName);
+        txtDiachi =(TextView)findViewById(R.id.txtDiachi);
+        imgHinh =(ImageView)findViewById(R.id.imageHinh);
+
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
+
+
+            }
+        });
+
+
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Picasso.with(this).load(result.getContents()).into(imgHinh);
+                try {
+                    JSONObject JsonObject = new JSONObject(result.getContents());
+                    txtDiachi.setText(JsonObject.getString("DiaChi"));
+                    txtName.setText(JsonObject.getString("name"));
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
     }
 }
